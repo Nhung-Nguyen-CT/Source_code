ab21dbe64117b60381406a54ba649ba459957ec9
Tuyet Nhung <tnhung27793@gmail.com>
2023-10-18 22:44:23 +0700

    finish excercise

--- Data_cleaning_IBM.py
+++ Data_cleaning_IBM.py
@@ -45,3 +45,52 @@ print('Skewness: %f' % log_transformed_lot.skew())
 
 #Handling the Duplicates
 duplicate = housing[housing.duplicated(['PID'])]
+#remove duplicated rows
+dup_removed = housing.drop_duplicates()
+#delete duplicated Order ID
+dup_order = housing[housing.duplicated(['Order'])]
+dup_order_removed = dup_order[dup_order.drop_duplicates(subset = ['Order'])]
+
+#Handling with missing values
+total_missing = housing.isnull().sum().sort_values(ascending = False)
+total_missing_select = total_missing.head(20)
+total_missing_select.plot(kind = 'bar', figsize = (8,6), fontsize = 10)
+plt.xlabel('Columns', fontsize = 20)
+plt.ylabel('Count', fontsize = 20)
+plt.title('Total Missing values', fontsize = 20)
+plt.show()
+#delete rows with Lot Frontage missing values
+housing.dropna(subset = ['Lot Frontage'])
+#delete column: Lot Frontage:
+housing.dropna(subset = ['Lot Frontage'])
+#replace with median
+median = housing['Lot Frontage'].median()
+housing['Lot Frontage'].fillna(value = median, inplace = True)
+housing['Lot Frontage'].isnull().sum()
+
+#Feature Scaling
+# normalization: Min-max scaling:
+#values are shifted and rescaled, ranging from 0 to 1. by subtracting the min value and dividing by the max minus min
+norm_data = MinMaxScaler().fit_transform(hous_num)
+norm_data
+# Standardization: to scale into standard distribution: shifted by mean, devide by standard deviation
+scaled_data = StandardScaler().fit_transform(hous_num)
+scaled_data
+#standardization for 1 column:
+scaled_price = StandardScaler().fit_transform(hous_num['SalePrice'][:,np.newaxis])
+
+#Handling with outliers
+sns.boxplot(x=housing['Lot Area'])
+sns.boxplot(x=housing['SalePrice'])
+price_area = housing.plot.scatter(x='Gr Liv Area',
+                      y='SalePrice')
+plt.show()
+#deleting outliers
+housing.sort_values(by = 'Gr Liv Area', ascending = False)[:2]
+outliers_dropped = housing.drop(housing.index[[1499,2181]])
+new_plot = outliers_dropped.plot.scatter(x='Gr Liv Area',
+                                         y='SalePrice')
+plt.show()
+#finding outlier by z-score
+housing['L_stats'] = stats.zscore(housing['Low Qual Fin SF'])
+outliers = housing[housing['L_stats'] > 3]
